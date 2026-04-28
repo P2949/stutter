4a1f06bbadb6a54de309a0a269791144206cad1e
Torby Pedro <106978220+P2949@users.noreply.github.com>
2026-04-29 00:44:02 +0100

    Add shared drop counter constants

--- stutter-common/src/lib.rs
+++ stutter-common/src/lib.rs
@@ -2,6 +2,10 @@
 
 pub const EVENT_RUNNABLE_LATENCY: u32 = 1;
 
+pub const DROP_WAKEUP_TIMES_INSERT_FAILED: u32 = 0;
+pub const DROP_RINGBUF_RESERVE_FAILED: u32 = 1;
+pub const DROP_COUNTERS_MAX: u32 = 2;
+
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct SchedulerEvent {
