53e7d779b306e10a80c9dc29f922e1ae473e78c3
P2949 <torbypedro@gmail.com>
2026-05-09 23:41:24 +0100

    refactor: move LiveTelemetry and TuiRenderSnapshot to dedicated module files

--- stutter/src/session.rs
+++ stutter/src/session.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{BTreeMap, BTreeSet, HashSet, VecDeque},
+    collections::{BTreeMap, BTreeSet, HashSet},
     fs,
     path::Path,
     sync::Arc,
@@ -24,11 +24,11 @@ use crate::{
     process_tree::{self, find_auto_target_pids},
     psi,
     recorder::{
-        self, BlockIoRecord, FinalizeRecordingInput, FrameEvent, GpuSample, IrqEventRecord,
-        JsonArrayWriter, LiveRecorder, SpikeEvent, SpikeEventBuffer,
+        self, FinalizeRecordingInput, JsonArrayWriter, LiveRecorder, SpikeEvent, SpikeEventBuffer,
     },
     runtime_slices::RuntimeSliceSampler,
     scx,
+    session::{live_telemetry::LiveTelemetry, ui::TuiRenderSnapshot},
     session_events::MonitorEvent,
     tasks::TaskTracker,
     watch::{
@@ -56,98 +56,6 @@ pub mod ui;
 const LIVE_DIAGNOSIS_CLUSTER_WINDOW_MS: u64 = 5;
 // Keep this aligned with the report default unless live diagnosis gets
 // its own CLI/config field.
-
-pub struct LiveTelemetry {
-    pub spikes: VecDeque<SpikeEvent>,
-    pub irq_events: VecDeque<IrqEventRecord>,
-    pub gpu_samples: VecDeque<GpuSample>,
-    pub io_events: VecDeque<BlockIoRecord>,
-    pub frame_events: VecDeque<FrameEvent>,
-    pub diagnoses: VecDeque<LiveDiagnosisEntry>,
-    pub max_age_ms: u128,
-}
-
-impl LiveTelemetry {
-    pub fn push_spike(&mut self, event: SpikeEvent) {
-        self.spikes.push_back(event);
-    }
-
-    pub fn push_irq(&mut self, event: IrqEventRecord) {
-        self.irq_events.push_back(event);
-    }
-
-    pub fn push_gpu(&mut self, sample: GpuSample) {
-        self.gpu_samples.push_back(sample);
-    }
-
-    pub fn push_io(&mut self, event: BlockIoRecord) {
-        self.io_events.push_back(event);
-    }
-
-    pub fn push_frame(&mut self, event: FrameEvent) {
-        self.frame_events.push_back(event);
-    }
-
-    pub fn prune(&mut self, now_ms: u128) {
-        while self.spikes.front().is_some_and(|s| {
-            now_ms.saturating_sub(s.elapsed_ms.unwrap_or(0).into()) > self.max_age_ms
-        }) {
-            self.spikes.pop_front();
-        }
-
-        while self.irq_events.front().is_some_and(|e| {
-            now_ms.saturating_sub(e.elapsed_ms.unwrap_or(0).into()) > self.max_age_ms
-        }) {
-            self.irq_events.pop_front();
-        }
-
-        while self
-            .gpu_samples
-            .front()
-            .is_some_and(|s| now_ms.saturating_sub(s.elapsed_ms.into()) > self.max_age_ms)
-        {
-            self.gpu_samples.pop_front();
-        }
-
-        while self
-            .io_events
-            .front()
-            .is_some_and(|e| now_ms.saturating_sub(e.elapsed_ms.into()) > self.max_age_ms)
-        {
-            self.io_events.pop_front();
-        }
-
-        while self
-            .frame_events
-            .front()
-            .is_some_and(|e| now_ms.saturating_sub(e.elapsed_ms.into()) > self.max_age_ms)
-        {
-            self.frame_events.pop_front();
-        }
-
-        while self
-            .diagnoses
-            .front()
-            .is_some_and(|d| now_ms.saturating_sub(d.elapsed_ms.into()) > self.max_age_ms)
-        {
-            self.diagnoses.pop_front();
-        }
-    }
-}
-
-impl Default for LiveTelemetry {
-    fn default() -> Self {
-        LiveTelemetry {
-            spikes: VecDeque::new(),
-            irq_events: VecDeque::new(),
-            gpu_samples: VecDeque::new(),
-            io_events: VecDeque::new(),
-            frame_events: VecDeque::new(),
-            diagnoses: VecDeque::new(),
-            max_age_ms: 10_000,
-        }
-    }
-}
 
 fn needs_tree_tick_from_parts(
     had_tree_roots: bool,
@@ -339,21 +247,6 @@ mod foreground_session_tests {
 
         assert!(!foreground_identity_changed(Some(&old), &new));
     }
-}
-
-#[derive(Clone)]
-pub(crate) struct TuiRenderSnapshot {
-    pub(crate) elapsed_ms: u64,
-    pub(crate) drop_counters: crate::ebpf_loader::DropCountersSnapshot,
-    pub(crate) tui_state: crate::tui::TuiState,
-    pub(crate) active_targets: std::collections::BTreeMap<u32, crate::process_tree::TaskInfo>,
-    pub(crate) stats_by_task: std::collections::BTreeMap<u32, crate::metrics::TaskStats>,
-    pub(crate) interval_records: Vec<crate::recorder::IntervalRecord>,
-    pub(crate) recent_diagnoses: std::collections::VecDeque<crate::diagnosis::LiveDiagnosisEntry>,
-    pub(crate) current_focus: Option<ResolvedFocus>,
-    pub(crate) current_foreground: Option<crate::foreground::ForegroundWindowSnapshot>,
-    pub(crate) focus_switch_count: u64,
-    pub(crate) foreground_include_title: bool,
 }
 
 pub struct MonitorSession {
--- stutter/src/session/ui.rs
+++ stutter/src/session/ui.rs
@@ -1,5 +1,3 @@
-#![allow(dead_code)]
-
 #[derive(Clone)]
 pub(crate) struct TuiRenderSnapshot {
     pub(crate) elapsed_ms: u64,
